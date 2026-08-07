b61730db77e9ab332686b71f005705e0844da7fd
Alick <144825686+alick-karoun@users.noreply.github.com>
2026-08-07 17:50:39 +0400

    Modify AWS region and add environment variable
    Updated AWS region and added environment variable while removing unused variables.

--- variable.tf
+++ variable.tf
@@ -1,37 +1,17 @@
-//customize the VPC CIDR, domain name, and subnets
-
 variable "aws_region" {
   type        = string
-  default     = "us-east-1"
-  description = "AWS Region for deployment"
+  default     = "eu-north-1"
+  description = "AWS deployment region"
 }
 
-variable "project_name" {
+variable "environment" {
   type        = string
-  default     = "my-app"
-  description = "Project prefix for resource tagging"
+  default     = "dev"
+  description = "Deployment environment"
 }
 
 variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
-  description = "Base CIDR block for the custom VPC"
-}
-
-variable "public_subnet_cidrs" {
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
-  description = "CIDR blocks for Public Subnets"
-}
-
-variable "private_subnet_cidrs" {
-  type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24"]
-  description = "CIDR blocks for Private Subnets"
-}
-
-variable "domain_name" {
-  type        = string
-  default     = "example.com" # Replace with your actual domain name
-  description = "Domain name for the Route53 Hosted Zone"
+  description = "CIDR block for the VPC"
 }
